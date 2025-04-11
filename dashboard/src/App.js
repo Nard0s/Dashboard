a288a34b8c16776e2ab85853725d828cd1a7ea3c
nard0s <ninanteneh20@gmail.com>
2025-04-11 13:31:27 +0300

    app updated

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,25 @@
-
+import React,{useEffect} from 'react';
+import{BrowserRouter , Routes , Route} from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent} from '@syncfusion/ej2-react-popups';
 import './App.css';
 function App() {
   return (
-    <div className="underline text-3x1">
-     app
+    <div>
+     <BrowserRouter>
+     <div className='flex relative dark:bg-main-dark-bg'>
+        <div className='fixed right-4 bottom-4'style={{zIndex:"1000"}}>
+          <TooltipComponent content="setting" position='Top'>
+            <button type='button' className='text-3x1 p-3 hover:drop-shadow-x1 hover : bg-light-grey text-white'
+              style={{background:'blue',
+                borderRadius:'50%'
+              }}>
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+     </div>
+     </BrowserRouter>
     </div>
   );
 }
