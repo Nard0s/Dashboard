5765589342435a625a1e8321c1fee5efcf38b4df
nard0s <ninanteneh20@gmail.com>
2025-04-12 17:29:58 +0300

    side bar routes added

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -28,14 +28,41 @@ function App() {
             <div className='w-0 bg-secondary-dark-bg'>
             Sidebar w-0
           </div>
-          )
-        }
+          )}
+
         <div className={
-          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu?' md:m1-72 ':'flex-2'}`
+          `dark:bg-main-bg bg-main-bg min-h-screen w-full ${ activeMenu? 'md:m1-72' : 'flex-2'}`
         }>
           <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
             Navbar
           </div>
+        </div>
+
+        <div>
+            <Routes>
+              {/* DaShboard pages */}
+              <Route path ="/" element="ECommerce"/>
+              <Route path ="/ecommerce" element="ECommerce"/>
+              {/* pages */}
+              <Route path ='/orders'  element="Orders"/>
+              <Route path ='/employees'  element="Employees"/>
+              <Route path ='/customers'  element="Customers"/>
+              {/* apps */}
+              <Route path ='/kanban'  element="Kanban"/>
+              <Route path ='/editor'  element="Editor"/>
+              <Route path ='/calender'  element="Calendar"/>
+              <Route path ='/color-picker'  element="ColorPicker"/>
+              {/* chart */}
+              <Route path ='/line'  element="Line"/>
+              <Route path ='/area'  element="Area"/>
+              <Route path ='/bar'  element="Bar"/>
+              <Route path ='/pie'  element="Pie"/>
+              <Route path ='/financial'  element="Financial"/>
+              <Route path ='/color-mapping'  element="ColorMapping"/>
+              <Route path ='/pyramid'  element="Pyramid"/>
+              <Route path ='/stacked'  element="Stacked"/>
+              
+            </Routes>
         </div>
      </div>
      </BrowserRouter>
