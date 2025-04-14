aef1d2bca1b78d4c9532551af25d5557b625245c
nard0s <ninanteneh20@gmail.com>
2025-04-14 22:17:46 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React,{useEffect} from 'react';
 import{BrowserRouter , Routes , Route} from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent} from '@syncfusion/ej2-react-popups';
+import {Navbar , Footer, Sidebar, ThemeSetting} from './components';
+import{ Ecommerce, Orders, Calendar, Employee, Stacked, Pie, Pyramid, ColorPicker, Customers, Area, Bar, Financial, Kanban,ColorMapping, Editor} from './pages'
 import './App.css';
 function App() {
 
@@ -12,7 +14,7 @@ function App() {
      <div className='flex relative dark:bg-main-dark-bg'>
         <div className='fixed right-4 bottom-4'style={{zIndex:"1000"}}>
           <TooltipComponent content="setting" position='Top'>
-            <button type='button' className='text-3x1 p-3 hover:drop-shadow-x1 hover : bg-light-grey text-white'
+            <button type='button' className='text-3xl p-3 hover:drop-shadow-x1 hover : bg-light-grey text-white'
               style={{background:'blue',
                 borderRadius:'50%'
               }}>
